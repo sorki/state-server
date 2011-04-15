b1f8bf6de96d4833c56cec6086c8540140d55f10
Richard Marko <rissko@gmail.com>
2011-04-15 17:07:47 +0200

    Separate dirs

---
+++ src/core/state.py
@@ -0,0 +1,14 @@
+class State(object):
+    __state_dict = {}
+
+    def set(self, name, value):
+        self.__state_dict[name] = value
+
+    def get(self, name):
+        if name in self.__state_dict:
+            return self.__state_dict[name]
+        else:
+            return None
+
+    def dump(self):
+        return self.__state_dict
