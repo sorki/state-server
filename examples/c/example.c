1809f0d72d0b293756d6f6361ae1dc8f10f2d1f9
Richard Marko <rissko@gmail.com>
2011-04-17 00:47:26 +0200

    C interface & example

---
+++ examples/c/example.c
@@ -0,0 +1,20 @@
+#include<string.h>
+#include<stdio.h>
+#include "state.h"
+
+int main(int argc, char *argv[]) {
+    const int sockfd = init_socket();
+    char buffer[256];
+    bzero(buffer, 256);
+
+    state_update(sockfd, "testvar", "value");
+    if(state_query(sockfd, "testvar", buffer) == 0)
+      printf("%s", buffer);
+
+    state_update(sockfd, "testvar", "new_value");
+    if(state_query(sockfd, "testvar", buffer) == 0)
+      printf("%s", buffer);
+
+    close_socket(sockfd);
+    return 0;
+}
